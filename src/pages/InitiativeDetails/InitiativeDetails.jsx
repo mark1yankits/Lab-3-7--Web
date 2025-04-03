8372fc262dab7aca62c39bba02a949f09f77e71b
mark1yankits <marcusik6509@gmail.com>
2025-04-04 02:03:31 +0300

    Joined functional btn

--- src/pages/InitiativeDetails/InitiativeDetails.jsx
+++ src/pages/InitiativeDetails/InitiativeDetails.jsx
@@ -13,8 +13,38 @@ function InitiativeDetails() {
         setInitiative(selectedInitiative);
     }, [id]);
 
-    if (!initiative) return <p>Завантаження...</p>; 
+    const sendItem = ()=>{
+        const storedInitiatives = JSON.parse(localStorage.getItem("initiatives") || "[]");
+        const initiativeIndex  = storedInitiatives.findIndex(item => item.id === initiative.id);
+        if (initiativeIndex !== -1 && storedInitiatives[initiativeIndex].volunteersNeeded > 0) {
+            storedInitiatives[initiativeIndex].volunteersNeeded -= 1;
 
+            const updatedInitiatives = storedInitiatives.map((item, index) =>
+                index === initiativeIndex
+                    ? { ...item, volunteersNeeded: item.volunteersNeeded - 1 }
+                    : item
+            );
+            localStorage.setItem("initiatives", JSON.stringify(updatedInitiatives));
+            setInitiative(updatedInitiatives[initiativeIndex]);
+
+            const selectedInitiative = {
+                id: updatedInitiatives[initiativeIndex].id,
+                title: updatedInitiatives[initiativeIndex].title,
+                date: updatedInitiatives[initiativeIndex].date,
+                description: updatedInitiatives[initiativeIndex].description,
+                img: updatedInitiatives[initiativeIndex].img,
+                volunteersNeeded: updatedInitiatives[initiativeIndex].volunteersNeeded
+            }
+            localStorage.setItem("selectedInitative", JSON.stringify(selectedInitiative));
+            
+            alert("Ви успішно приєдналися до ініціативи!");
+
+        } 
+        else{
+            alert("Всі волонтери вже набрані для цієї ініціативи!");
+        }
+    }
+    if (!initiative) return <p>Завантаження...</p>;
     return (
         <main>
             <div className="container__detailInit">
@@ -29,7 +59,7 @@ function InitiativeDetails() {
                     <p className="date__detail__text">{initiative.date}</p>
                     <p className="text__detail__text">{initiative.description}</p>
                     <p className="volunteers__needed">Потрібно волонтерів: <span className="volunteers-count">{initiative.volunteersNeeded}</span></p>
-                    <button className="detail__btn">{initiative.volunteersNeeded > 0 ? 'Відгукнутись!' : 'Приєднались!'}</button>
+                    <button onClick = {sendItem} className="detail__btn">{initiative.volunteersNeeded > 0 ? 'Відгукнутись!' : 'Приєднались!'}</button>
                 </div>
             </div>
         </main>
